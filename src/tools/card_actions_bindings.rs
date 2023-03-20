22c0e0d43acc92737a32151cbfbac7885b9e9380
alexis <abeloos@dupuis.com>
2023-03-20 09:11:52 +0100

    put it all in one block

--- src/tools/card_actions_bindings.rs
+++ src/tools/card_actions_bindings.rs
@@ -1,22 +1,20 @@
+#![allow(non_upper_case_globals)]
+
 use wasm_bindgen::prelude::*;
 use web_sys::{NodeList, HtmlElement};
 
 #[wasm_bindgen]
-extern {
-    pub type HTMLDocument;
-    pub static document: HTMLDocument;
-    #[wasm_bindgen(method, js_name = querySelector)]
-    pub fn query_selector(this: &HTMLDocument, selector: &str) -> Option<HtmlElement>;
-    #[wasm_bindgen(method, js_name = querySelectorAll)]
-    pub fn query_selector_all(this: &HTMLDocument, selector: &str) -> NodeList;
-}
-
-#[wasm_bindgen]
 extern "C" {
+    pub type HtmlDocument;
+    pub static document: HtmlDocument;
+    #[wasm_bindgen(method, js_name = querySelector)]
+    pub fn query_selector(this: &HtmlDocument, selector: &str) -> Option<HtmlElement>;
+    #[wasm_bindgen(method, js_name = querySelectorAll)]
+    pub fn query_selector_all(this: &HtmlDocument, selector: &str) -> NodeList;
+
     // Log MouseEvent
     #[wasm_bindgen(js_namespace = console)]
     pub fn log(s: &str);
-
     #[wasm_bindgen(js_namespace = console)]
     pub fn error(s: JsValue);
 }
