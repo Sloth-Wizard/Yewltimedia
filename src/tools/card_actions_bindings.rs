fd9890d9325b12c4097fd026870bd5035e91465d
Alexis Abeloos <35430441+Sloth-Wizard@users.noreply.github.com>
2023-04-07 09:45:05 +0200

    card_actions_bindings doc
    Explain what everything does

--- src/tools/card_actions_bindings.rs
+++ src/tools/card_actions_bindings.rs
@@ -1,3 +1,5 @@
+//! Rust wasm bindings
+
 #![allow(non_upper_case_globals)]
 
 use wasm_bindgen::prelude::*;
@@ -5,16 +7,21 @@ use web_sys::{NodeList, HtmlElement};
 
 #[wasm_bindgen]
 extern "C" {
+    /// Document type
     pub type HtmlDocument;
+    /// The Document element
     pub static document: HtmlDocument;
+    /// querySelector() js binding
     #[wasm_bindgen(method, js_name = querySelector)]
     pub fn query_selector(this: &HtmlDocument, selector: &str) -> Option<HtmlElement>;
+    /// querySelectorAll() js binding
     #[wasm_bindgen(method, js_name = querySelectorAll)]
     pub fn query_selector_all(this: &HtmlDocument, selector: &str) -> NodeList;
 
-    // Log MouseEvent
+    /// Log MouseEvent
     #[wasm_bindgen(js_namespace = console)]
     pub fn log(s: &str);
+    /// console.error
     #[wasm_bindgen(js_namespace = console)]
     pub fn error(s: JsValue);
 }
